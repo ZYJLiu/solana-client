48055f2f419f64902589d3937d892ecb97038cd1
John <johnzhengyuliu@gmail.com>
2023-10-05 14:04:25 -0500

    top-level await

--- src/main.ts
+++ src/main.ts
@@ -6,45 +6,43 @@ import {
   clusterApiUrl,
   sendAndConfirmTransaction,
 } from "@solana/web3.js";
-import { getOrCreateKeypair, airdropSolIfNeeded } from "./utils";
+import { airdropSolIfNeeded, getOrCreateKeypair } from "./utils.ts";
 
-(async () => {
-  // Establish a connection to the Solana devnet cluster
-  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+// Establish a connection to the Solana devnet cluster
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
-  // Use existing keypairs or generate new ones if they don't exist
-  const wallet_1 = await getOrCreateKeypair("wallet_1");
-  const wallet_2 = await getOrCreateKeypair("wallet_2");
+// Use existing keypairs or generate new ones if they don't exist
+const wallet_1 = await getOrCreateKeypair("wallet_1");
+const wallet_2 = await getOrCreateKeypair("wallet_2");
 
-  console.log(`\n`);
+console.log(`\n`);
 
-  // Request an airdrop of SOL to wallet_1 if its balance is less than 1 SOL
-  await airdropSolIfNeeded(wallet_1.publicKey);
+// Request an airdrop of SOL to wallet_1 if its balance is less than 1 SOL
+await airdropSolIfNeeded(wallet_1.publicKey);
 
-  // Define the amount to transfer
-  const transferAmount = 0.1; // 0.1 SOL
+// Define the amount to transfer
+const transferAmount = 0.1; // 0.1 SOL
 
-  // Create a transfer instruction for transferring SOL from wallet_1 to wallet_2
-  const transferInstruction = SystemProgram.transfer({
-    fromPubkey: wallet_1.publicKey,
-    toPubkey: wallet_2.publicKey,
-    lamports: transferAmount * LAMPORTS_PER_SOL, // Convert transferAmount to lamports
-  });
+// Create a transfer instruction for transferring SOL from wallet_1 to wallet_2
+const transferInstruction = SystemProgram.transfer({
+  fromPubkey: wallet_1.publicKey,
+  toPubkey: wallet_2.publicKey,
+  lamports: transferAmount * LAMPORTS_PER_SOL, // Convert transferAmount to lamports
+});
 
-  // Add the transfer instruction to a new transaction
-  const transaction = new Transaction().add(transferInstruction);
+// Add the transfer instruction to a new transaction
+const transaction = new Transaction().add(transferInstruction);
 
-  // Attempt to send and confirm the transaction
-  try {
-    await sendAndConfirmTransaction(connection, transaction, [wallet_1]);
-  } catch (error) {
-    console.error("Transaction unsuccessful: ", error);
-  }
+// Attempt to send and confirm the transaction
+try {
+  await sendAndConfirmTransaction(connection, transaction, [wallet_1]);
+} catch (error) {
+  console.error("Transaction unsuccessful: ", error);
+}
 
-  // Retrieve and log the new balance of each wallet after the transfer
-  const balance1 = await connection.getBalance(wallet_1.publicKey);
-  console.log("wallet_1 new balance:", balance1 / LAMPORTS_PER_SOL);
+// Retrieve and log the new balance of each wallet after the transfer
+const balance1 = await connection.getBalance(wallet_1.publicKey);
+console.log("wallet_1 new balance:", balance1 / LAMPORTS_PER_SOL);
 
-  const balance2 = await connection.getBalance(wallet_2.publicKey);
-  console.log("wallet_2 new balance:", balance2 / LAMPORTS_PER_SOL);
-})();
+const balance2 = await connection.getBalance(wallet_2.publicKey);
+console.log("wallet_2 new balance:", balance2 / LAMPORTS_PER_SOL);
