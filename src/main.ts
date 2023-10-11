4feccedddc020d91e48c1850facf1ee29e72e4e4
John <johnzhengyuliu@gmail.com>
2023-10-11 11:43:25 -0500

    fix comment

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 // Use existing keypairs or generate new ones if they don't exist
 const wallet_1 = await getKeypairFromEnvironment("wallet_1");
 
-// Retrieve and log the new balance of each wallet after the transfer
+// Fetch the lamport balance for the specified public key
 const balance = await connection.getBalance(wallet_1.publicKey);
 console.log("wallet_1 balance:", balance / LAMPORTS_PER_SOL);
