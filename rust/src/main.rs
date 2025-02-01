0845f7f9a5feae6e88fc30faec73affa585d71ee
John <johnzhengyuliu@gmail.com>
2025-02-01 15:36:47 -0600

    rust

---
+++ rust/src/main.rs
@@ -0,0 +1,31 @@
+use solana_client::rpc_client::RpcClient;
+use solana_sdk::{commitment_config::CommitmentConfig, native_token::LAMPORTS_PER_SOL, signer::Signer};
+use anyhow::Result;
+
+mod utils;
+use utils::get_or_create_keypair;
+
+fn main() -> Result<()> {
+    let wallet_1 = get_or_create_keypair("wallet_1")?;
+    println!("wallet_1 address: {}", wallet_1.pubkey());
+
+    let client =
+        RpcClient::new_with_commitment(String::from("http://127.0.0.1:8899"), CommitmentConfig::confirmed());
+    
+    let transaction_signature = client.request_airdrop(&wallet_1.pubkey(), 5 * LAMPORTS_PER_SOL)?;
+
+    loop {
+       if client.confirm_transaction(&transaction_signature)? {
+        break;
+       }
+    }
+
+    let balance = client.get_balance(&wallet_1.pubkey())?;
+    println!("wallet_1 balance: {}", balance / LAMPORTS_PER_SOL);
+
+    println!(
+        "Transaction Signature: https://explorer.solana.com/tx/{}?cluster=custom",
+        transaction_signature
+    );
+    Ok(())
+}
---
+++ rust/src/utils.rs
@@ -0,0 +1,24 @@
+use anyhow::Result;
+use solana_sdk::signer::keypair::Keypair;
+use std::{env, fs::OpenOptions, io::Write};
+
+pub fn get_or_create_keypair(variable_name: &str) -> Result<Keypair> {
+    dotenv::dotenv().ok();
+
+    if let Ok(secret_key_string) = env::var(variable_name) {
+        let decoded_secret_key: Vec<u8> = serde_json::from_str(&secret_key_string)?;
+        return Ok(Keypair::from_bytes(&decoded_secret_key)?);
+    }
+
+    let keypair = Keypair::new();
+    let secret_key_bytes = keypair.to_bytes().to_vec();
+    let json_secret_key = serde_json::to_string(&secret_key_bytes)?;
+
+    OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(".env")?
+        .write_all(format!("{}={}\n", variable_name, json_secret_key).as_bytes())?;
+
+    Ok(keypair)
+}
