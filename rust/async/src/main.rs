71d7642e88ab8bf427ed8924df92032d36b75918
John <75003086+ZYJLiu@users.noreply.github.com>
2025-04-20 16:16:16 -0500

    restructure and update dependencies

--- rust/async/src/main.rs
+++ rust/async/src/main.rs
@@ -1,6 +1,8 @@
-use solana_client::nonblocking::rpc_client::RpcClient;
-use solana_sdk::{commitment_config::CommitmentConfig, native_token::LAMPORTS_PER_SOL, signer::Signer};
 use anyhow::Result;
+use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_sdk::{
+    commitment_config::CommitmentConfig, native_token::LAMPORTS_PER_SOL, signer::Signer,
+};
 
 mod utils;
 use utils::get_or_create_keypair;
@@ -10,15 +12,19 @@ async fn main() -> Result<()> {
     let wallet_1 = get_or_create_keypair("wallet_1").await?;
     println!("wallet_1 address: {}", wallet_1.pubkey());
 
-    let client =
-        RpcClient::new_with_commitment(String::from("http://127.0.0.1:8899"), CommitmentConfig::confirmed());
-    
-    let transaction_signature = client.request_airdrop(&wallet_1.pubkey(), 5 * LAMPORTS_PER_SOL).await?;
+    let client = RpcClient::new_with_commitment(
+        String::from("http://127.0.0.1:8899"),
+        CommitmentConfig::confirmed(),
+    );
+
+    let transaction_signature = client
+        .request_airdrop(&wallet_1.pubkey(), 5 * LAMPORTS_PER_SOL)
+        .await?;
 
     loop {
-       if client.confirm_transaction(&transaction_signature).await? {
-        break;
-       }
+        if client.confirm_transaction(&transaction_signature).await? {
+            break;
+        }
     }
 
     let balance = client.get_balance(&wallet_1.pubkey()).await?;
